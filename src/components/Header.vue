ca96084dcc79c070bfd28e0ec15983e19fe65a50
Shkelqim Beselica <shkelqim@gjirafa.com>
2021-10-15 12:39:08 +0200

    First commit

---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul class="header__nav">
+    <li class="header__title" v-for="(link, idx) of links" :key="idx">
+      <a
+        class="header__link"
+        :href="link.route"
+        :class="{ 'text__orange text__bold': link.orange }"
+      >
+        {{ link.name }}
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      links: [
+        {
+          name: "Work",
+          route: "#work",
+        },
+        {
+          name: "Resume",
+          route: "#resume",
+        },
+        {
+          name: "About me",
+          route: "#about",
+        },
+        {
+          name: "Get in touch",
+          route: "#contact",
+          orange: true,
+        },
+      ],
+    };
+  },
+};
+</script>
