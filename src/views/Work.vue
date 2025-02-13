26cb98ba73e1a0384686bd5163632e4e9b7c6d32
Mergim Ujkani <mergim.ujkani@gmail.com>
2025-02-13 11:35:30 +0100

    Completed UI for work in homepage
    UI for work in home page completed,
    fixed responsive for home page

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -8,8 +8,8 @@
 
     <div class="case__studies-section">
       <CaseStudy
-        v-for="study in caseStudies"
-        :key="study.id"
+        v-for="(study, index) in caseStudies"
+        :key="index"
         :caseStudy="study"
         @showWork="show = true"
       />
@@ -30,92 +30,86 @@ export default {
 
       caseStudies: [
         {
-          titleTag: "DESKTOP",
+          titleTag: "DESKTOP, TABLET & MOBILE",
           titleTagColor: "#66ABF7",
+          workInfo: "Senior UX/UI · 2020 - Present",
+          name: "BAMF Clinic and Radiopharmacy",
+          description:
+            "BAMF (Bold Advanced Medical Future) Clinic and Radiopharmacy are platforms tailored exclusively for the needs of doctors and technicians to build sensitive drugs, manage inventory, and treat patients. ",
+          route: "bamf-clinic",
+          src: require("@/assets/images/work-images/bamf-clinic.webp"),
+        },
+        {
+          titleTag: "DESKTOP",
+          titleTagColor: "#636CE5",
           workInfo: "Design Lead · 2021 - 2022",
           name: "Thrive - Core & Activity app",
-          id: 1,
           description:
             "Thrive is a community based, web3 platform for people to contribute and share skills with one another by exchanging the platform's signature coin — thrivecoin.",
           route: "thrive",
-          src: require("@/assets/images/work/Thrive.png"),
+          src: require("@/assets/images/work-images/thrivecoin.webp"),
         },
         {
-          titleTag: "DESKTOP",
-          titleTagColor: "#66ABF7",
-          workInfo: "Design Lead · 2021 - 2022",
-          name: "BAMF Clinic Portal",
-          id: 2,
+          titleTag: "DESKTOP & MOBILE",
+          titleTagColor: "#63C6A0",
+          workInfo: "Senior UX/UI · 2020 - Present",
+          name: "BAMF Patient",
           description:
-            "Thrive is a community based, web3 platform for people to contribute and share skills with one another by exchanging the platform's signature coin — thrivecoin.",
-          route: "bamf-clinic",
-          src: require("@/assets/images/work_new/bamf.png"),
-        },
-        {
-          titleTag: "DESKTOP",
-          titleTagColor: "#66ABF7",
-          workInfo: "Design Lead · 2021 - 2022",
-          name: "BAMF Patient Portal",
-          id: 3,
-          description:
-            "Thrive is a community based, web3 platform for people to contribute and share skills with one another by exchanging the platform's signature coin — thrivecoin.",
+            "BAMF (Bold Advanced Medical Future) Patient is a platform for allowing patients with Metastasized Neuroendocrine or Prostate cancer to apply for a novel treatment in one of their clinics.",
           route: "bamf-patient",
-          src: require("@/assets/images/work/BAMF.png"),
+          src: require("@/assets/images/work-images/bamf-patient.webp"),
         },
+
         {
-          titleTag: "DESKTOP",
-          titleTagColor: "#66ABF7",
-          workInfo: "Design Lead · 2021 - 2022",
+          titleTag: "DESKTOP & MOBILE",
+          titleTagColor: "#D79B70",
+          workInfo: "UX/UI Designer and Illustrator · 2019 - 2020",
           name: "Uniseguros platform",
-          id: 4,
           description:
-            "Thrive is a community based, web3 platform for people to contribute and share skills with one another by exchanging the platform's signature coin — thrivecoin.",
+            "Uniseguros is a Panamanian service company that acts as a middle man for clients and insurance companies. Its main goal is to simplify the experience when applying, managing or making a claim or a quote. ",
           route: "uniseguros-platform",
-          src: require("@/assets/images/work/UnisegurosPlatform.png"),
+          src: require("@/assets/images/work-images/uniseguros-platform.webp"),
         },
         {
-          titleTag: "DESKTOP",
-          titleTagColor: "#66ABF7",
-          workInfo: "Design Lead · 2021 - 2022",
+          titleTag: "DESKTOP & MOBILE",
+          titleTagColor: "#FFCF68",
+          workInfo: "UX/UI Designer and Illustrator · 2019 - 2020",
           name: "Uniseguros website",
-          id: 5,
           description:
-            "Thrive is a community based, web3 platform for people to contribute and share skills with one another by exchanging the platform's signature coin — thrivecoin.",
+            "Uniseguros website is focused on a seamless quote creation experience while maintaining a similar design language to the platform. ",
           route: "uniseguros-website",
-          src: require("@/assets/images/work/UnisegurosWebsite.png"),
+          src: require("@/assets/images/work-images/bamf-clinic.webp"),
         },
+
         {
-          titleTag: "DESKTOP",
-          titleTagColor: "#66ABF7",
-          workInfo: "Design Lead · 2021 - 2022",
-          name: "Balkan Cuisine",
-          id: 6,
-          description:
-            "Thrive is a community based, web3 platform for people to contribute and share skills with one another by exchanging the platform's signature coin — thrivecoin.",
-          route: "balkan-cuisine",
-          src: require("@/assets/images/work/BalkanCuisine.png"),
-        },
-        {
-          titleTag: "DESKTOP",
-          titleTagColor: "#66ABF7",
-          workInfo: "Design Lead · 2021 - 2022",
-          name: "Express",
-          id: 7,
-          description:
-            "Thrive is a community based, web3 platform for people to contribute and share skills with one another by exchanging the platform's signature coin — thrivecoin.",
-          route: "express",
-          src: require("@/assets/images/work/BalkanCuisine.png"),
-        },
-        {
-          titleTag: "DESKTOP",
-          titleTagColor: "#66ABF7",
-          workInfo: "Design Lead · 2021 - 2022",
+          titleTag: "MOBILE",
+          titleTagColor: "#92D739",
+          workInfo: "UX/UI Designer and Illustrator · 2018",
           name: "Friendict",
-          id: 8,
           description:
-            "Thrive is a community based, web3 platform for people to contribute and share skills with one another by exchanging the platform's signature coin — thrivecoin.",
+            "Frendict is a mobile betting app for predicting soccer game results against friends and strangers using the platform's coin system.",
           route: "friendict",
-          src: require("@/assets/images/work/Friendict.png"),
+          src: require("@/assets/images/work-images/friendict.webp"),
+        },
+        {
+          titleTag: "DESKTOP & MOBILE",
+          titleTagColor: "#FB8787",
+          workInfo: "UX/UI Designer - 2018",
+          name: "Express",
+          description:
+            "Express was a redesign project for one of the largest digital medias in Kosovo.",
+          route: "express",
+          src: require("@/assets/images/work-images/express.webp"),
+        },
+        {
+          titleTag: "DESKTOP & MOBILE",
+          titleTagColor: "#E6936F",
+          workInfo: "UX/UI Designer and Illustrator · 2017",
+          name: "Balkan Cuisine",
+          description:
+            "Balkan Cuisine is a curation of Balkan's finest dishes. It was an early personal, side project that’s close to my heart ❤️.",
+          route: "balkan-cuisine",
+          src: require("@/assets/images/work-images/balkan-cousine.webp"),
         },
       ],
     };
